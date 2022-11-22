7d66307b7eecce1799791c5f2f1a4fed1a3369d4
Sky Yao <challenge.cool@gmail.com>
2022-11-22 18:25:54 +0800

    feat: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
+  rules: {}
+}
