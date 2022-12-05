9ded0d59c4f60a1f5aa801b7c1a0d59c79a93ed0
Sky Yao <challenge.cool@gmail.com>
2022-12-05 13:10:22 +0800

    feat: path alias config

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,13 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+const path = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') }
+  },
   plugins: [
     vue(),
     // Run ESLint while vite is running
